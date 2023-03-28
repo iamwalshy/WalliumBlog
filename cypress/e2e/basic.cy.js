8acb47d72aac63840606100a04133a0636fe0daa
iamwalshy <iamwalshy@protonmail.com>
2023-03-28 02:36:13 -0400

    renaming title of sitev2

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,6 +4,6 @@ describe('sample render test', () => {
   });
 
   it('displays the title text', () => {
-    cy.get('h1').contains('Wallium Tweaks and Tips');
+    cy.get('h1').contains('anyTweak');
   });
 });
